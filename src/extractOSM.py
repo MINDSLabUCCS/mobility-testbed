39d7cc72c5006760a794df915cc6e6b2864a6130
jcuberdruid <44100876+jcuberdruid@users.noreply.github.com>
2022-06-03 14:45:41 -0600

    modified osm.pbf variable names in config

--- src/extractOSM.py
+++ src/extractOSM.py
@@ -6,9 +6,9 @@ import asyncio
 
 async def extract(outputDir, coordinates):
 	path = os.path.join(outputDir, "map_"+os.path.basename(outputDir).split("_")[1]+".osm")
-	osmPbfFile = os.path.join(conf.gen["absPath"], conf.gen["userDir"], conf.osm["osmPbfFile"])
+	mapFile = os.path.join(conf.gen["absPath"], conf.gen["userDir"], conf.osm["mapFile"])
 	coordinates = coordinates.replace('\n','')
-	call = f'osmium extract -b {coordinates} {osmPbfFile} --strategy=smart -o {path} >> /dev/null'
+	call = f'osmium extract -b {coordinates} {mapFile} --strategy=smart -o {path} >> /dev/null'
 	proc = await asyncio.create_subprocess_shell(call, stdout=asyncio.subprocess.PIPE)
 	stdout,stderr = await proc.communicate()
 	return stdout
